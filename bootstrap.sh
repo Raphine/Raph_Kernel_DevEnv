ca391b7ad558cc2786b7adfc5c4697e844f0119b
Shinichi Awamoto <shinichi.awamoto@eure.jp>
2015-12-03 23:11:28 +0900

    bug fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,6 @@
 test -f /etc/bootstrapped && exit
 
 #sudo apt-get update
-sudo apt-get install -y git g++ make parted emacs language-pack-ja-base language-pack-ja kpartx
+sudo apt-get install -y git g++ make parted emacs language-pack-ja-base language-pack-ja kpartx qemu
 sudo update-locale LANG=ja_JP.UTF-8 LANGUAGE="ja_JP:ja"
 date > /etc/bootstrapped
