ecbda9ac5944f4f2549f44af7b4156a8c8644d9e
Shinichi Awamoto <shinichi.awamoto@eure.jp>
2015-12-03 23:04:57 +0900

    change to vagrant

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+test -f /etc/bootstrapped && exit
+
+#sudo apt-get update
+sudo apt-get install -y git g++ make parted emacs language-pack-ja-base language-pack-ja kpartx
+sudo update-locale LANG=ja_JP.UTF-8 LANGUAGE="ja_JP:ja"
+date > /etc/bootstrapped
