7d3ed33c60dab6506cbfaed2c6571c932d5b9783
Shinichi Awamoto <shinichi.awamoto@eure.jp>
2015-12-07 18:35:19 +0900

    change Vagrantfile

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y git g++ make parted emacs language-pack-ja-base language
 sudo update-locale LANG=ja_JP.UTF-8 LANGUAGE="ja_JP:ja"
 
 # install qemu
-sudo apt-get install -y libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev libncurses5-dev
+sudo apt-get install -y libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev
 wget http://wiki.qemu-project.org/download/qemu-2.4.1.tar.bz2
 tar xvf qemu-2.4.1.tar.bz2
 mkdir build-qemu
