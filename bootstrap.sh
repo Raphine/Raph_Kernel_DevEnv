5c11e736bbb0a86aceb05d8fd63e1e7588f4ee0c
Shinichi Awamoto <shinichi.awamoto@eure.jp>
2015-12-09 13:59:02 +0900

    apt update before install

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 test -f /etc/bootstrapped && exit
 
-#sudo apt-get update
+sudo apt-get update
 sudo apt-get install -y git g++ make parted emacs language-pack-ja-base language-pack-ja kpartx gdb
 sudo update-locale LANG=ja_JP.UTF-8 LANGUAGE="ja_JP:ja"
 
