7c65cb90bb56b3179f8c608d1626300af768cfb4
Liva <sap.pcmail@gmail.com>
2016-12-07 17:00:06 +0900

    bug fix & performance improvement

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,40 +1,40 @@
 #!/bin/sh
-test -f /etc/bootstrapped && exit
+sudo -sh c 'test -f /etc/bootstrapped && exit'
 
-apt-get update
-apt-get install -y git g++ make parted emacs language-pack-ja-base language-pack-ja kpartx gdb bridge-utils libyaml-dev
-update-locale LANG=ja_JP.UTF-8 LANGUAGE="ja_JP:ja"
+sudo apt-get update
+sudo apt-get install -y git g++ make parted emacs language-pack-ja-base language-pack-ja kpartx gdb bridge-utils libyaml-dev
+sudo update-locale LANG=ja_JP.UTF-8 LANGUAGE="ja_JP:ja"
 
 # install qemu
-apt-get install -y libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev
+sudo apt-get install -y libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev
 wget http://wiki.qemu-project.org/download/qemu-2.4.1.tar.bz2
 tar xvf qemu-2.4.1.tar.bz2
 mkdir build-qemu
 cd build-qemu
 ../qemu-2.4.1/configure --target-list=x86_64-softmmu --disable-kvm --enable-debug
-make
+make -j2
 make install
 cd ..
 
-(sudo -u vagrant curl -sSf https://static.rust-lang.org/rustup.sh | sh) || return 0
-echo -e "export PATH=\$PATH:~/.cargo/bin\n" >> /home/vagrant/.bashrc
-sudo -u vagrant cargo install rustfmt
+(curl -sSf https://static.rust-lang.org/rustup.sh | sh) || return 0
+echo "export PATH=\$PATH:~/.cargo/bin\n" >> /home/vagrant/.bashrc
+cargo install rustfmt --verbose
 
 # setup bridge initialize script
-sed -i -e 's/exit 0//g' /etc/rc.local
-echo "ifconfig eth0 down" >> /etc/rc.local
-echo "ifconfig eth0 up" >> /etc/rc.local
-echo "ip addr flush dev eth0" >> /etc/rc.local
-echo "brctl addbr br0" >> /etc/rc.local
-echo "brctl stp br0 off" >> /etc/rc.local
-echo "brctl setfd br0 0" >> /etc/rc.local
-echo "brctl addif br0 eth0" >> /etc/rc.local
-echo "ifconfig br0 up" >> /etc/rc.local
-echo "dhclient br0" >> /etc/rc.local
-echo "exit 0" >> /etc/rc.local
-/etc/rc.local
+sudo sed -i -e 's/exit 0//g' /etc/rc.local
+sudo sh -c 'echo "ifconfig eth0 down" >> /etc/rc.local'
+sudo sh -c 'echo "ifconfig eth0 up" >> /etc/rc.local'
+sudo sh -c 'echo "ip addr flush dev eth0" >> /etc/rc.local'
+sudo sh -c 'echo "brctl addbr br0" >> /etc/rc.local'
+sudo sh -c 'echo "brctl stp br0 off" >> /etc/rc.local'
+sudo sh -c 'echo "brctl setfd br0 0" >> /etc/rc.local'
+sudo sh -c 'echo "brctl addif br0 eth0" >> /etc/rc.local'
+sudo sh -c 'echo "ifconfig br0 up" >> /etc/rc.local'
+sudo sh -c 'echo "dhclient br0" >> /etc/rc.local'
+sudo sh -c 'echo "exit 0" >> /etc/rc.local'
+sudo /etc/rc.local
 
-mkdir /usr/local/etc/qemu
-echo "allow br0" > /usr/local/etc/qemu/bridge.conf
+sudo mkdir /usr/local/etc/qemu
+sudo sh -c 'echo "allow br0" > /usr/local/etc/qemu/bridge.conf'
 
-date > /etc/bootstrapped
+sudo sh -c 'date > /etc/bootstrapped'
